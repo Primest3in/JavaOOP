8140dd25bc215b0ad2d69c2201fd2a351a7d7273
Mahbubul Hasan <52739974+mahbubsust@users.noreply.github.com>
2020-04-24 00:19:00 +0600

    constructor extended

---
+++ constructor1.java
@@ -0,0 +1,59 @@
+class A
+{
+    A()
+    {
+        System.out.println("0 arg cons of class A");
+    }
+    A(int a)
+    {
+        System.out.println("1 arg cons of class A");
+    }
+    
+    
+}
+class B extends A
+{
+    B()
+    {
+        System.out.println("0 arg cons class B");
+    }
+    public static void main(String[] args) {
+        new B();
+    }
+    
+}
+/*
+output: 
+0 arg cons of class A **only 0 arg cons of parent will excute**
+0 arg cons of class B
+*/
+class C
+{
+    {
+        System.out.println("class C instance block");
+    }
+    C() {
+        System.out.println("class C 0 arg cons");
+    }
+}
+class D extends C
+{
+    {
+        System.out.println("class D instance block");
+    }
+    static {
+        System.out.println("class D static block");
+    }
+    public static void main(String[] args) {
+        new C();
+    }
+    
+
+
+}
+/*
+output:
+class D static block
+class C instance block
+0 arg cons class C
+*/
